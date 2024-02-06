6d70585bd55388f57cf803440d3e23ca76a14c2f
rafiee <rafiee110q@gmail.com>
2024-02-06 17:04:05 +0330

    craet file 01

---
+++ 01/01.js
@@ -0,0 +1,12 @@
+console.log("hello world");
+let a=10;
+let b=5;
+console.log("sum",a+b);
+let x=["ali",10,20];
+console.log(x);
+console.log(x[0]);
+x={
+mycar:"bmw",
+myspeed:100
+};
+console.log(x.mycar);
